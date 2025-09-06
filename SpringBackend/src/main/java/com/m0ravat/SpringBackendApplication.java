85deeb9d570d896bb5c408b8f9d7525adab1ed9a
Muhammad Ravat <m0ravat736@gmail.com>
2025-09-06 19:11:57 +0100

    started  basic backend in springboot

---
+++ SpringBackend/src/main/java/com/m0ravat/SpringBackendApplication.java
@@ -0,0 +1,14 @@
+package com.m0ravat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBackendApplication {
+
+    public static void main(String[] args) {
+
+        SpringApplication.run(SpringBackendApplication.class, args);
+    }
+
+}
